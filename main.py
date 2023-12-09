df5c190d2f3d8b7019b723a51d4df8de35e0ae23
Dan Sakamoto <contact@dansakamoto.com>
2023-12-09 01:16:02 -0500

    set headers and root url

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-
 from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+rootUrl = "https://taskmaster.info/show.php?id=1"
+
+html_demo = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+}
+req = Request(rootUrl, headers=headers)
+html_page = urlopen(req).read()
+
+soup = BeautifulSoup(html_page, "html.parser")
+
+print(soup.prettify())
