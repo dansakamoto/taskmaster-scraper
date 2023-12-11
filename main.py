cbfbaa96a72f93820fbd29a4bf306664c1d7361a
Dan Sakamoto <contact@dansakamoto.com>
2023-12-10 19:47:44 -0500

    remove demo html

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ from urllib.request import Request, urlopen
 
 rootUrl = "https://taskmaster.info/show.php?id=1"
 
-html_demo = """<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
 headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
 }
