04fd69dadc9ef699014f9a44be0426fde733af01
Dan Sakamoto <contact@dansakamoto.com>
2024-01-10 14:52:59 -0500

    step 3 completed

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from time import sleep
 import random, json, time, os
 
 # CONFIG
-debug = True
-baseUrl = "https://taskmaster.info"
+baseUrl = "https://taskmaster.info" 
 rootUrl = "https://taskmaster.info/show.php?id=1"
 blacklist = ["Edinburgh Does... Taskmaster"]
 headers = {
@@ -17,56 +16,81 @@ req = Request(rootUrl, headers=headers)
 html_page = urlopen(req).read()
 soup = BeautifulSoup(html_page, "lxml")
 
+allSeasons = {}
+
+# DEBUG
+skipTo = 3
+if skipTo > 0:
+    debugData = f"data/step{skipTo-1}_results.json"
+    with open(debugData, "r") as f:
+        allSeasons = json.load(f)
+        f.close()
+
 """
 STEP 1: collect seasons
 """
-soup = soup.select("#seasonsandspecials > div.season > p.seasonTitle > a")
-seasons = {}
-print("Loading seasons list...")
-for r in soup:
-    if r.string in blacklist:
-        continue
-    season = {}
-    season["url"] = baseUrl + r["href"]
-    seasons[r.string] = season
+if skipTo <= 1:
+    soup = soup.select("#seasonsandspecials > div.season > p.seasonTitle > a")
+    print("Loading seasons list...")
+    for r in soup:
+        if r.string in blacklist:
+            continue
+        season = {}
+        season["url"] = baseUrl + r["href"]
+        allSeasons[r.string] = season
 
 """
 STEP 2: collect episodes
 """
-for k, s in seasons.items():
-    sleep(random.uniform(2, 5))
-    print("\nScanning " + k)
-    req = Request(s["url"], headers=headers)
-    html_page = urlopen(req).read()
-    soup = BeautifulSoup(html_page, "html.parser")
-    soup2 = soup.select("#episodes")
-    isSpecial = len(soup2) == 0
+if skipTo <= 2:
+    unaired = []
+    for k, s in allSeasons.items():
+        sleep(random.uniform(2, 5))
+        print("\nScanning " + k)
+        req = Request(s["url"], headers=headers)
+        html_page = urlopen(req).read()
+        soup = BeautifulSoup(html_page, "html.parser")
+        soup2 = soup.select("#episodes")
+        isSpecial = len(soup2) == 0
 
-    episodes = []
-    if isSpecial:
-        soup2 = soup.select("#tasks")
-        isUnaired = len(soup2) == 0
-        if isUnaired:
-            print("(unaired, skipping the rest...)")
-            break
-        print("(it's a special)")
-        episodes.append({"title": "(special)", "url": s["url"]})
-    else:
-        soup = soup.select("#episodes > div.episode > p.episodeTitle > a")
-        for a in soup:
-            print(f"found episode: {a.string}")
-            episodes.append({"title": a.string, "url": baseUrl + a["href"]})
-    seasons[k]["episodes"] = episodes
+        episodes = []
+        if isSpecial:
+            soup2 = soup.select("#tasks")
+            isUnaired = len(soup2) == 0
+            if isUnaired:
+                print("(unaired...)")
+                unaired.append(k)
+                continue
+            print("(it's a special)")
+            episodes.append({"title": "(special)", "url": s["url"]})
+        else:
+            soup = soup.select("#episodes > div.episode > p.episodeTitle > a")
+            for a in soup:
+                print(f"found episode: {a.string}")
+                episodes.append({"title": a.string, "url": baseUrl + a["href"]})
+        allSeasons[k]["episodes"] = episodes
 
-"""
-TO DO:  if a season is unaired, it should be deleted from the dictionary,
-        instead of passed on to step 3
-"""
+    for k in unaired:
+        allSeasons.pop(k)
 
 """
 STEP 3: collect tasks
-TBC
 """
+if skipTo <= 3:
+    for skey, svalue in allSeasons.items():
+        for episode in svalue["episodes"]:
+            sleep(random.uniform(2,5))
+            print("\nScanning " + episode["title"])
+            req = Request(episode["url"], headers=headers)
+            html_page = urlopen(req).read()
+            soup = BeautifulSoup(html_page, "html.parser")
+            soup2 = soup.select("#tasks > div.task > div.taskInfo > div.taskCardTaskTitle > h3.taskLink > a")
+            tasks = []
+            for item in soup2:
+                print(f"found task: {item.string}\nurl: {item['href']}")
+                tasks.append({"title": item.string, "url": baseUrl + item["href"]})
+            episode["tasks"] = tasks
+        
 
 """
 STEP 4: collect task data
@@ -85,6 +109,6 @@ fName = f"outputs/out-{time.time()}.json"
 os.makedirs(os.path.dirname(fName), exist_ok=True)
 print("\nWriting results to file " + fName)
 with open(fName, "w") as f:
-    f.write(json.dumps(seasons, indent=2, ensure_ascii=False))
+    f.write(json.dumps(allSeasons, indent=2, ensure_ascii=False))
     f.close()
 print("\nDone!")
