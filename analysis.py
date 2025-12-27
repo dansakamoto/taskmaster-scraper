c096ad953223676234e8c9ccd786790581b0ba1f
Dan Sakamoto <contact@dansakamoto.com>
2025-12-26 22:17:35 -0500

    analysis update

--- analysis.py
+++ analysis.py
@@ -4,33 +4,26 @@ from time import sleep
 import random, json, time, os
 
 # Load results from previous step
-existingData = f"data/cleanup_results.json"
+existingData = f"data/final_results.json"
 with open(existingData, "r") as f:
     allVersions = json.load(f)
     f.close()
 
-elements = {}
+elements = []
 
 # Iterate through all attempts, find attempt descriptions
 for versionKey, version in allVersions.items():
     for seasonKey, season in version["seasons"].items():
         for episodeKey, episode in season["episodes"].items():
             for taskKey, task in episode["tasks"].items():
-                if "notes" not in task:
-                    continue
-
-                soup = BeautifulSoup(task["description"], "html.parser")
-
-                struct = ""
-                soup = soup.find("p")
-
-                if str(soup) not in elements:
-                    elements[str(soup)] = 1
-                else:
-                    elements[str(soup)] += 1
+                for t in task["taskTypes"]:
+                    if t not in elements:
+                        elements.append(t)
 
 
-for k, v in elements.items():
-    if v > 10:
-        print(v)
-        print(k)
+res = ""
+for e in elements:
+    res += '"' + e + '" | '
+
+res = res[:-3]
+print(res)
